f982042045a3225136c40f2208263aa7d89b851d
Reza1290 <reza.muktasib@gmail.com>
2024-12-22 22:42:56 +0900

    update: monorepo

--- apis/userApi.ts
+++ apis/userApi.ts
@@ -1,12 +1,7 @@
 const API_BASE_URL = 'http://localhost:5000/api'
 import { InterfaceUser } from '../../shared/logic/userInterface';
 
-export interface User extends Omit<InterfaceUser, 'name' | 'email' | 'password'> {
-  id?: string;
-  name: string;
-  email: string;
-  password: string;
-}
+export interface User extends Omit<InterfaceUser, 'name' | 'email' | 'password'> {}
 
 
 interface ResponseData {
